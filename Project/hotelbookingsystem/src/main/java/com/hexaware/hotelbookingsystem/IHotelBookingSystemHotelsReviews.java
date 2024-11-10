b3f7e667bc82d56ae8a91db00cfa84a51bb31845
PrernaSamalia <128030079+PrernaSamalia@users.noreply.github.com>
2024-11-10 20:19:05 +0530

    Update and rename IHotelBookingSystemHotelsReviews to IHotelBookingSystemHotelsReviews.java

--- Project/hotelbookingsystem/src/main/java/com/hexaware/hotelbookingsystem/IHotelBookingSystemHotelsReviews.java
+++ Project/hotelbookingsystem/src/main/java/com/hexaware/hotelbookingsystem/IHotelBookingSystemHotelsReviews.java
@@ -3,7 +3,7 @@ package com.hexaware.hotelbookingsystem.service;
 import com.hexaware.hotelbookingsystem.entity.Reviews;
 import java.util.List;
 
-public interface IHotelBookingSystemHotelsReviews {
+public interface IHotelBookingSystemReviewsService {
     Reviews addReview(Reviews review);
     Reviews getReviewById(Integer reviewId);
     List<Reviews> getReviewsByHotelId(Integer hotelId);
